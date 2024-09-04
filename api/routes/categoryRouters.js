2ca9ae6a030c1b38da9f8d868f9a173495facbf2
Daniel <danielcaz@gmail.com>
2024-09-04 01:21:51 -0300

    falta el endpoint post para items, falla

--- api/routes/categoryRouters.js
+++ api/routes/categoryRouters.js
@@ -8,8 +8,8 @@ const {
 const router = express.Router();
 
 router.get("/categories/:id", async (req, res) => {
-    try {
-      const { id } = req.params;    
+  try {
+    const { id } = req.params;
     const categories = await findCategoryById(id);
     if (!categories) {
       res.status(400).json({
@@ -19,31 +19,27 @@ router.get("/categories/:id", async (req, res) => {
       res.status(200).json(categories);
     }
   } catch (error) {
-    res.status(500).json({ message: `Error al obtener la categoría con id:${id}` });
+    res
+      .status(500)
+      .json({ message: `Error al obtener la categoría con id:${id}` });
   }
 });
 
 router.get("/categories", async (req, res) => {
   try {
-    // res.send("lego acaaaaa")
     const categories = await getAllCategories();
-    res.json(categories);
+    res.status(201).res.json(categories);
   } catch (error) {
-    console.log("entraaaaaaaaaaaaa");
-    res.status(500).json({ message: "Error al obtener las categoríasssss" });
+    res.status(500).json({ message: "Error al obtener las categorías"});
   }
 });
 
-
 // Endpoint para crear una nueva categoría
 router.post("/categorie", async (req, res) => {
-  console.log("entra antes del try?");
   try {
-    console.log("despues del try");
     const newCategory = await createCategory(req.body);
     res.status(201).json(newCategory);
   } catch (error) {
-    console.log("entro al errrorr");
     res.status(400).json({ error: error.message });
   }
 });
--- api/routes/itemsRouters.js
+++ api/routes/itemsRouters.js
@@ -1,7 +1,16 @@
 const express = require('express');
-const { createItem } = require('../services/itemsServices'); // Ajusta la ruta según tu estructura
+const { createItem, getAllItems } = require('../services/itemsServices'); // Ajusta la ruta según tu estructura
 
 const router = express.Router();
+
+router.get('/items', async (req, res) =>{    
+    try {
+        const items = await getAllItems()
+        res.status(201).json(items);
+    } catch (error) {
+        res.status(500).json({ message: "Error al obtener los platos" }); 
+    }
+})
 
 // Endpoint para crear un nuevo ítem
 router.post('/items', async (req, res) => {
