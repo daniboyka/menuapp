c8902d646839b6728e98f0c2a26695d7028a61f8
emanuelgimenez2 <emanuelgimenez2@gmail.com>
2024-09-11 20:30:30 -0300

    modificacion agregado cors en el back

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const connectDB = require('./config/db'); 
+const cors = require('cors');
 require('dotenv').config({ path: '../.env' });
+
 
 const categoryRoutes = require('./routes/categoryRouters'); // Importa las rutas de categorías
 const itemsRoutes = require('./routes/itemsRouters'); // Importa las rutas de platos
@@ -11,6 +13,7 @@ const PORT = process.env.PORT || 3001;
 connectDB();
 // Middleware para rutas
 app.use(express.json()); // Asegúrate de poder manejar JSON si es necesario
+app.use(cors());
 
 // Rutas
 app.use('/api', categoryRoutes);
@@ -19,6 +22,30 @@ app.use('/api', itemsRoutes);
 app.get("/api", (req, res) => {
     res.send('el servidor esta funcionoando anda')
   });
+
+
+
+
+
+
+
+
+ 
+
+
+
+// Habilitar CORS para todas las solicitudes
+
+
+// Opcional: Si deseas permitir solo ciertas URLs
+// app.use(cors({ origin: 'http://localhost:3000' }));
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log(`estoy ejecutando en http://localhost:${PORT}`);
